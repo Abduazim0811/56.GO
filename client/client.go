ba4dc0209f716baddffc3b078a75c78c409bd1e1
Abduazim <yusupovabduazim0811@gmail.com>
2024-06-14 08:15:22 +0500

    first

--- client/client.go
+++ client/client.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	addr := flag.String("addr", "localhost:50051", "the address to connect to")
+	addr := flag.String("addr", "localhost:12345", "the address to connect to")
 	flag.Parse()
 
 	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -21,15 +21,34 @@ func main() {
 	}
 	defer conn.Close()
 
-	c:=pb.NewStringServiceClient(conn)
+	c := pb.NewStringServiceClient(conn)
 
-	ctx, cancel :=context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	// Call tolower
+
+	input := "Hello, gRPC!"
+
 	r, err := c.ToUpper(ctx, &pb.StringRequest{Input: input})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not convert to upper: %v", err)
 	}
 	log.Printf("ToUpper: %s", r.Output)
 
+	r, err = c.ToLower(ctx, &pb.StringRequest{Input: input})
+	if err != nil {
+		log.Fatalf("could not convert to lower: %v", err)
+	}
+	log.Printf("ToLower: %s", r.Output)
+
+	r, err = c.Reverse(ctx, &pb.StringRequest{Input: input})
+	if err != nil {
+		log.Fatalf("could not reverse: %v", err)
+	}
+	log.Printf("Reverse: %s", r.Output)
+
+	lenResp, err := c.Length(ctx, &pb.StringRequest{Input: input})
+	if err != nil {
+		log.Fatalf("could not get length: %v", err)
+	}
+	log.Printf("Length: %d", lenResp.Length)
 }
